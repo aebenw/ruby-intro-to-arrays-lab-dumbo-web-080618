9a82f16d23be4ee7b7a0b5581e5c7cecaedd4b22
Andrew Woodward <ebenwoodward@icloud.com>
2018-07-19 05:09:14 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -24,4 +24,10 @@ def first_element_with_array_methods(arr)
   return arr.first 
 end
 
-def first_element_with_array_methods
+def last_element_with_array_methods(arr)
+  return arr.last
+end
+
+def length_of_array(arr)
+  return arr.length 
+end
